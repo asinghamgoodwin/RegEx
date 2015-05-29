8a0ad719fe68cf06ae496fb3550cce4fd63360c6
asinghamgoodwin <asinghamgoodwin@gmail.com>
2015-05-29 13:05:52 +0000

    adding in OR

--- regexMatching.py
+++ regexMatching.py
@@ -1,7 +1,7 @@
 ######createStatesMASTER makes a list of states (with their transitions) from  an expression######
 ######Things left to do: add a language class, add a penny/transitioning system, deal with |########
 
-from regexParens import parens, findBuddy
+from regexParens import parens, parensPLUS, findBuddy
 
 
 #I decided to make Language a class so that if you had a bunch of strings to check in the same language
@@ -10,7 +10,7 @@ from regexParens import parens, findBuddy
 class Language():
     def __init__(self, alphabet, expression):
         self.alphabet = alphabet #a list with each character in the alphabet
-        self.expression = parens(alphabet, expression) #parens adds in the appropriate parenthesis for the NFA maker
+        self.expression = parensPLUS(alphabet, expression) #parens adds in the appropriate parenthesis for the NFA maker
                                                         #should only include letters in self.alphabet and (,),|,*,.
         self.states = createStatesMASTER(self.alphabet, self.expression) #list with all of the states in the machine
 
@@ -24,16 +24,47 @@ class Language():
                 nextStates.add(state.outDict[character])
 
             currentStates = nextStates #now these are are current states...
-            nextStates = set()
+            #nextStates = set()
+
+
+            def freeTransitions(state):
+                possibleNewStates = set()
+                y = state #y will get updated at each state we step through on a free transition to ensure that we get as far as possible
+                while 'free' in y.outDict.keys():
+                    if type(y.outDict['free']) == list: #if you could transition freely to multiple states
+                        for newState in y.outDict['free']:
+                            possibleNewStates.add(newState) #put each of them into the nextStates set
+                        branch0 = y.outDict['free'][0]
+                        branch1 = y.outDict['free'][1]
+                        y = branch0 #keep the first one and keep going through this process of checking for free transitions
+                        z = branch1
+                        possibleNewStates.union(freeTransitions(z))
+                    else:
+                        y = y.outDict['free']
+                    possibleNewStates.add(y)
+                return possibleNewStates
+
+
 
             for state in currentStates: #...but we also have to deal with free transitions.
                                         #Keep every state in the set, but add every state we /could/ get to through a free transition
-                y = state #y will get updated at each state we step through on a free transition to ensure that we get as far as possible
-                while 'free' in y.outDict.keys():
-                    y = y.outDict['free']
-                    nextStates.add(y)
-            currentStates = currentStates.union(nextStates) #now add the states we got to freely to the states we got from the character transition
- 
+                currentStates = currentStates.union(freeTransitions(state))
+
+
+##
+##                y = state #y will get updated at each state we step through on a free transition to ensure that we get as far as possible
+##                while 'free' in y.outDict.keys():
+##                    if type(y.outDict['free']) == list: #if you could transition freely to multiple states
+##                        for newState in y.outDict['free']:
+##                            nextStates.append(newState) #put each of them into the nextStates set
+##                        y = y.outDict['free'][0] #keep the first one and keep going through this process of checking for free transitions
+##                        z = y.outDict['free'][1]
+##                        #########how to I deal with adding in more states through free transitions for the other branch???########
+##                    else:
+##                        y = y.outDict['free']
+##                    nextStates.add(y)
+##            currentStates = currentStates.union(nextStates) #now add the states we got to freely to the states we got from the character transition
+## 
         for state in currentStates:
             if state.acceptState:
                 return True
@@ -61,7 +92,10 @@ class State():
     def prettyDict(self):
         prettyDict = {}
         for key in self.outDict.keys():
-            prettyDict[key] = self.outDict[key].name
+            if type(self.outDict[key]) == list:
+                prettyDict[key] = [self.outDict[key][0].name, self.outDict[key][1].name]
+            else:
+                prettyDict[key] = self.outDict[key].name
         return prettyDict
 
 
@@ -77,7 +111,7 @@ def createStates(alphabet, expression, holding, trap, statesList, holdingSTAR=No
     holding = holding
     holdingSTAR = holdingSTAR
 
-    if expression[0] == "(" and findBuddy(expression, 0) == len(expression)-1:
+    if expression[0] == "(" and findBuddy(expression, 0) == len(expression)-1 and expression[-2] != "*":
         expression = expression[1:-1] #we can take out parenthases if they enclose the whole machine
     
     i = 0
@@ -110,17 +144,44 @@ def createStates(alphabet, expression, holding, trap, statesList, holdingSTAR=No
                 holding.outDict['free'] = star
 
                 holding = star
+
+            elif expression[i+1] == "|" and expression[i+2] == "|": #if this set of parens encases an OR
+                #the expression has to be in the following format, starting at i:  (||(m1)|(m2))
+
+                m1close = findBuddy(expression, i+3)
+                separator = m1close+1
+                    
+                m1expression = expression[i+3:m1close+1]
                 
+                m1helper = State(names.pop(), alphabet, trap)
+                statesList.append(m1helper)
+                m1output = createStates(alphabet, m1expression, m1helper, trap, statesList)
+
+
+                m2expression = expression[separator+1:close]
+                
+                m2helper = State(names.pop(), alphabet, trap)
+                statesList.append(m2helper)
+                m2output = createStates(alphabet, m2expression, m2helper, trap, statesList)
+
+                holding.outDict['free'] = [m1helper, m2helper]
+
+                endHelper = State(names.pop(), alphabet, trap)
+                statesList.append(endHelper)
+                
+                m1output.outDict['free'] = endHelper
+                m2output.outDict['free'] = endHelper
+
+                holding = endHelper                
+
+            
             else:     
                 output = createStates(alphabet, expression[i+1:close], holding, trap, statesList)
                 holding = output
             
             i = close+1
 
-##        elif x == "|":
-##            pass
-          
-    
+
     return holding
 
 
@@ -154,14 +215,37 @@ def match(alphabet, expression, string):
 
 binary = ['0', '1']
 
+A = "1.*(01)*1(000|1*)"
 B = "1(1(00)*1)*"
 C = ".(11(0*))*(1*)" #didn't work with an extra * at the end.....why??
 E = "1(011*)*."
+F = "1(00|101)"
+
+##aCheck1 = '111111000'
+##aCheck2 = '1111110000'
+##aCheck3 = '1001111'
+
+##print match(binary, A, aCheck1) == True
+##print match(binary, A, aCheck2) == False
+##print match(binary, A, aCheck3) == True
+
+##aLang = Language(binary, A)
+
+
+##fCheck1 = "1101" #true
+##fCheck2 = "100101" #false
+##fCheck3 = "110" #false
+
+##fLang = Language(binary, F)
+
+##print match(binary, F, fCheck1) == True
+##print match(binary, F, fCheck2) == False
+##print match(binary, F, fCheck3) == False
 
 
 ##bLang = Language(binary, parens(binary, B))
 ##bCheck1 = '1100001' #true
-bCheck2 = '110000111' #true
+##bCheck2 = '110000111' #true
 ##bCheck3 = '11000011' #false
 ##bCheck4 = '11000010' #false
 ##bCheck5 = '110001' #false
@@ -172,7 +256,7 @@ bCheck2 = '110000111' #true
 ##    print bLang.checkString(s)
 
 
-print match(binary, B, bCheck2)
+##print match(binary, B, bCheck2)
 
 ##cLang = Language(binary, parens(binary, C))
 ##cCheck1 = '0110001' #true
@@ -192,8 +276,8 @@ print match(binary, B, bCheck2)
 #cList = createStatesMASTER(binary, parens(binary, C))
 #eList = createStatesMASTER(binary, parens(binary, E))
 
-
-##for state in bList:
+##
+##for state in aLang.states:
 ##    print state.name
 ##    if state.acceptState == True:
 ##            print "acceptState"
